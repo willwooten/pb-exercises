b19cb96f61edc9ea7f00057fef80ff43630bfe59
Will Wooten <42416243+willwooten@users.noreply.github.com>
2021-06-02 11:23:44 -0500

    session 6

--- session6/block.py
+++ session6/block.py
@@ -23,55 +23,107 @@ class Block:
         '''Takes a byte stream and parses a block. Returns a Block object'''
         # s.read(n) will read n bytes from the stream
         # version - 4 bytes, little endian, interpret as int
+        version = little_endian_to_int(s.read(4))
         # prev_block - 32 bytes, little endian (use [::-1] to reverse)
+        prev_block = s.read(32)[::-1]
         # merkle_root - 32 bytes, little endian (use [::-1] to reverse)
+        merkle_root = s.read(32)[::-1]
         # timestamp - 4 bytes, little endian, interpret as int
+        timestamp = little_endian_to_int(s.read(4))
         # bits - 4 bytes
+        bits = s.read(4)
         # nonce - 4 bytes
+        nonce = s.read(4)
         # initialize class
-        raise NotImplementedError
+        return cls(version, prev_block, merkle_root, timestamp, bits, nonce)
+
 
     def serialize(self):
         '''Returns the 80 byte block header'''
-        # version - 4 bytes, little endian
-        # prev_block - 32 bytes, little endian
-        # merkle_root - 32 bytes, little endian
-        # timestamp - 4 bytes, little endian
-        # bits - 4 bytes
         # nonce - 4 bytes
-        raise NotImplementedError
+        result = int_to_little_endian(self.version, 4)
+        # prev_block - 32 bytes, little endian
+        result += self.prev_block[::-1]
+        # merkle_root - 32 bytes, little endian
+        result += self.merkle_root[::-1]
+        # timestamp - 4 bytes, little endian
+        result += int_to_little_endian(self.timestamp, 4)
+        # bits - 4 bytes
+        result += self.bits
+        # version - 4 bytes, little endian
+        result += self.nonce
+        return result
 
     def hash(self):
         '''Returns the hash256 interpreted little endian of the block'''
         # serialize
+        s = self.serialize()
         # hash256
+        h256 = hash256(s)
         # reverse
-        raise NotImplementedError
+        return h256[::-1]
+
 
     def id(self):
         '''Human-readable hexadecimal of the block hash'''
         return self.hash().hex()
 
+    def bip9(self):
+        '''Returns whether this block is signaling readiness for BIP9'''
+        # BIP9 is signalled if the top 3 bits are 001
+        # remember version is 32 bytes so right shift 29 (>> 29) and see if
+        # that is 001
+        return self.version >> 29 == 0b001
+
+    def bip112(self):
+        '''Returns whether this block is signaling readiness for BIP112'''
+        # BIP112 is signalled if the first bit from the right is 1
+        # shift 0 bits to the right and see if the last bit is 1
+        return self.version >> 0 & 1 == 1
+
+    def bip141(self):
+        '''Returns whether this block is signaling readiness for BIP141'''
+        # BIP141 is signalled if the 2nd bit from the right is 1
+        # shift 1 bit to the right and see if the last bit is 1
+        return self.version >> 1 & 1 == 1
+
+    def bip341(self):
+        '''Returns whether this block is signaling readiness for BIP341'''
+        # BIP341 is signalled if the 3rd bit from the right is 1
+        # shift 2 bits to the right and see if the last bit is 1
+        return self.version >> 2 & 1 == 1
+
+    def bip91(self):
+        '''Returns whether this block is signaling readiness for BIP91'''
+        # BIP91 is signalled if the 5th bit from the right is 1
+        # shift 4 bits to the right and see if the last bit is 1
+        return self.version >> 4 & 1 == 1
+
     def target(self):
         '''Returns the proof-of-work target based on the bits'''
         # last byte is exponent
+        exponent = self.bits[-1]
         # the first three bytes are the coefficient in little endian
+        coefficient = little_endian_to_int(self.bits[:-1])
         # the formula is:
         # coefficient * 256**(exponent-3)
-        raise NotImplementedError
+        return coefficient * 256 ** (exponent-3)
 
     def difficulty(self):
         '''Returns the block difficulty based on the bits'''
         # note difficulty is (target of lowest difficulty) / (self's target)
         # lowest difficulty has bits that equal 0xffff001d
-        raise NotImplementedError
+        return (0xffff * 256 ** (0x1d - 3)) / self.target()
 
     def check_pow(self):
         '''Returns whether this block satisfies proof of work'''
         # get the hash256 of the serialization of this block
+        h256 = hash256(self.serialize())
         # interpret this hash as a little-endian number
+        result = little_endian_to_int(h256)
         # return whether this integer is less than the target
-        raise NotImplementedError
+        target = self.target()
+        return result < target
 
 
 class BlockTest(TestCase):
